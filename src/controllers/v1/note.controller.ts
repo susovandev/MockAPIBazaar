eaf808024f430cc857b93852e9270b2f7a14b9f8
Susovan Dev <susovandas.dev@gmail.com>
2025-07-17 11:12:13 +0530

    feat: update note controller to support string type for note IDs and enhance update functionality

--- src/controllers/v1/note.controller.ts
+++ src/controllers/v1/note.controller.ts
@@ -1,11 +1,14 @@
-import { TCreateNoteDTO, TResponseNoteDTO } from '@/dtos/index.js';
+import {
+    TCreateNoteDTO,
+    TResponseNoteDTO,
+    TUpdateNoteDTO,
+} from '@/dtos/index.js';
 import { noteServices } from '@/services/index.js';
 import { ApiResponse } from '@/utils/apiResponse.js';
 import { asyncHandler } from '@/utils/asyncHandler.js';
 import Logger from '@/utils/logger.js';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { ObjectId } from 'mongoose';
 
 class NoteController {
     createNote = asyncHandler(
@@ -29,7 +32,10 @@ class NoteController {
     );
 
     getNoteById = asyncHandler(
-        async (req: Request<{ id: ObjectId }>, res: Response) => {
+        async (
+            req: Request<{ id: string }>,
+            res: Response<ApiResponse<TResponseNoteDTO>>,
+        ) => {
             Logger.info(`Fetching note with id: ${req.params.id}`);
             const note = await noteServices.getNoteById(req.params.id);
 
@@ -37,6 +43,29 @@ class NoteController {
                 new ApiResponse(
                     StatusCodes.OK,
                     'Note fetched successfully',
+                    note,
+                ),
+            );
+        },
+    );
+
+    updateNoteById = asyncHandler(
+        async (
+            req: Request<{ id: string }, unknown, TUpdateNoteDTO>,
+            res: Response<ApiResponse<TResponseNoteDTO>>,
+        ) => {
+            Logger.info(`Updating note with id: ${req.params.id} with data
+                ${JSON.stringify(req.body)}`);
+
+            const note = await noteServices.updateNoteById(
+                req.params.id,
+                req.body,
+            );
+
+            res.status(StatusCodes.OK).json(
+                new ApiResponse(
+                    StatusCodes.OK,
+                    'Note updated successfully',
                     note,
                 ),
             );
